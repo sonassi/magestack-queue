814e53295bf91fe02450e40bce3867d8ad543011
Sonassi <contact@sonassi.com>
2017-09-18 21:11:32 +0100

    Disable module by default in configuration

--- config.php
+++ config.php
@@ -15,15 +15,15 @@ return [
     ],
 
     // Whether the queue system is enabled or not
-    'enabled' => true,
+    'enabled' => false,
 
     // Maximim number of users on site at any time
     // Set value to -1 to force everyone into the queue
-    'threshold' => 10,
+    'threshold' => 100,
 
     // Period of time a user can be idle in queue before being
     // kicked out (in seconds)
-    'timer' => 300,
+    'timer' => 600,
 
     // Google anayltics tracking code
     'ga_code' => '',
